5009db962378348c513c4b58a640ea51041d414b
laxitpdl <poudellakshit042061@gmail.com>
2025-09-13 15:01:45 -0500

    about

--- pages/About.py
+++ pages/About.py
@@ -1,5 +1,3 @@
 import streamlit as st
 
-st.write("Hello")
-
-#for pages in webapp 
+st.write("Hello, This is my first AI-Powered To-Do List application. It was built using Streamlit, LangChain, and Google's Gemini Pro model")
